198b0129c6c9e2f701c4f0d7df8879c090a6f9c2
Gerardo Ruiz <grville93@gmail.com>
2023-08-14 15:03:28 -0500

    MinHeap implementation tested succesfully

--- Data Structures/Heaps, Stacks and Queues/Heap_Imp_Testing.py
+++ Data Structures/Heaps, Stacks and Queues/Heap_Imp_Testing.py
@@ -1,88 +1,64 @@
-from Heap_Implementation import MaxHeap
+from Heap_Implementation import MaxHeap, MinHeap
 import random
 
-new_heap = MaxHeap()
+
+'MaxHeap Testing'
+# new_heap = MaxHeap()
 
 # [new_heap.insert(random.randint(0,11)) for _ in range(0,9)]
-[new_heap.insert(i) for i in [10, 10, 9, 5, 3, 1, 6, 0]]
+# # [new_heap.insert(i) for i in [10, 10, 9, 5, 3, 1, 6, 0]]
 
-new_heap.display()
+# new_heap.display()
 
-print()
+# print()
 
-new_heap.display_heap()
+# new_heap.display_heap()
 
 # print()
 # print(new_heap.find_min())
-print()
+# print()
 
-new_heap.delete(7)
-new_heap.display()
-print()
-
-new_heap.display_heap()
-
-
-
-
-
-# Heap display func under dev.
-
-# def display_heap(heap):
-       
-#     def get_level(index):
-#         return (index.bit_length() - 1) if index > 0 else 0
-
-#     def print_spaces(count):
-#         print(" " * count, end="")
-
-#     def print_branches(levels, current_level, index):
-#         if current_level >= levels:
-#             return
-
-#         elements_in_level = 2 ** current_level
-#         spaces_between_elements = 2 ** (levels - current_level) - 1
-
-#         print_spaces(spaces_between_elements)
-
-#         for i in range(elements_in_level):
-#             current_index = (2 ** current_level - 1) + i
-#             if current_index < len(heap):
-#                 print(f"{heap[current_index]:>4}", end="")
-#             else:
-#                 break
-
-#             if i < elements_in_level - 1:
-#                 print_spaces(spaces_between_elements * 2)
-
-#         print()
-
-#         next_level = current_level + 1
-#         for i in range(elements_in_level):
-#             current_index = (2 ** current_level - 1) + i
-#             left_child_index = current_index * 2 + 1
-#             right_child_index = current_index * 2 + 2
-
-#             if left_child_index < len(heap) and get_level(left_child_index) == next_level:
-#                 print_spaces(spaces_between_elements)
-#                 print_branches(levels, next_level, left_child_index)
-
-#             if right_child_index < len(heap) and get_level(right_child_index) == next_level:
-#                 print_spaces(spaces_between_elements)
-#                 print_branches(levels, next_level, right_child_index)
-
-#     height = get_level(len(heap) - 1) + 1
-#     print_branches(height, 0, 0)
+# new_heap.delete(8)
+# new_heap.display()
+# print()
+# new_heap.display_heap()
 
 
 
 
 
+'MinHeap Testing'
+
+# new_heap = MinHeap()
+
+# # [new_heap.insert(random.randint(0,11)) for _ in range(0,9)]
+# [new_heap.insert(i) for i in [5, 11, 6, 2, 3, 11, 6, 4, 2]]
+
+# new_heap.display()
+
+# # print()
+
+# # new_heap.display_heap()
+# print()
 
 
+# heap_min = new_heap.extract_min()
+
+# new_heap.display()
+
+# print()
+
+# new_heap.display_heap()
+# print()
+
+# print(heap_min)
 
 
+# print(new_heap.find_max())
 
 
+# new_heap.delete(4)
 
-
+# new_heap.display()
+# print()
+# new_heap.display_heap()
