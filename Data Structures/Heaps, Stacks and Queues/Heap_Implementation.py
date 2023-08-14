a979861cbda74d40074e3cf6b6506cb9917628fb
Gerardo Ruiz <grville93@gmail.com>
2023-08-14 14:44:26 -0500

    MinHeap Class added

--- Data Structures/Heaps, Stacks and Queues/Heap_Implementation.py
+++ Data Structures/Heaps, Stacks and Queues/Heap_Implementation.py
@@ -98,7 +98,7 @@ class MaxHeap:
 
         while index < len(self.heap):
 
-            # Taking all the child node from the next level
+            # Taking all the child nodes from the next level
             childs_range = [childs_range[0]*2+1, childs_range[1]*2+2]   
 
             # If the new upper bound is greater than the heap, take the last index of the heap instead
@@ -158,6 +158,231 @@ class MaxHeap:
             self._heapify_down(ind)
       
 
+    def pop(self):
+        
+        if not self.heap:
+            return
+        
+        popped = self.heap[-1]
+        self.heap = self.heap[:-1]
+
+        return popped
+
+
+    def display_heap(self):
+        
+        heap = self.heap
+        if not heap:
+            return
+
+        def get_level(index):
+            return (index.bit_length() - 1) if index > 0 else 0
+
+        def get_children_indices(parent_index):
+            left_child_index = 2 * parent_index + 1
+            right_child_index = 2 * parent_index + 2
+            return left_child_index, right_child_index
+
+        def print_spaces(count):
+            print(" " * count, end="")
+
+        height = get_level(len(heap) - 1) + 1
+        max_width = 2 ** height - 1
+
+        # Initialize queue with the root node
+        queue = [(0, 0)]  # (index, level)
+
+        current_level = 0
+        current_line_index = 0
+
+        while queue:
+            index, level = queue.pop(0)
+
+            if level > current_level:
+                # Move to the next line
+                print()
+                current_line_index = 0
+                current_level = level
+
+            if index < len(heap):
+                # Print the value
+                print_spaces(max_width // (2 ** (level + 1)))
+                print(f"{heap[index]:>4}", end="")
+            else:
+                # Print spaces for missing elements
+                print_spaces(max_width // (2 ** (level + 1) + 1))
+
+            current_line_index += 1
+
+            # Add children to the queue
+            left_child_index, right_child_index = get_children_indices(index)
+            if left_child_index < len(heap):
+                queue.append((left_child_index, level + 1))
+            if right_child_index < len(heap):
+                queue.append((right_child_index, level + 1))
+
+        print()  # Print a newline at the end
+
+
+
+class MinHeap: 
+
+    def __init__(self):
+
+        self.heap = []
+
+
+    def _index_handler(self, index):
+
+        if index > (self.heap_len()-1) or index < -self.heap_len():
+            raise IndexError('Index out of range')
+        
+        # converting the index into a positive index
+        if index < 0: 
+
+            norm_index = range(self.heap_len())
+            inv_index = range(-len(norm_index),0)
+            dict_index = {k:v for k,v in zip(inv_index, norm_index)}
+
+            return dict_index[index]
+
+        else:
+
+            return index   
+
+
+    def _heapify_up(self, index):
+
+        parent_index = (index - 1) // 2
+
+        while parent_index >= 0 and self.heap[index] < self.heap[parent_index]:
+
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+
+            index = parent_index
+            parent_index = (index - 1) // 2
+
+
+    def _heapify_down(self, index):
+
+        left_child_index = 2 * index + 1
+        right_child_index = 2 * index + 2
+        smallest = index
+
+        if left_child_index < len(self.heap) and self.heap[left_child_index] < self.heap[smallest]:
+            smallest = left_child_index
+
+        if right_child_index < len(self.heap) and self.heap[right_child_index] < self.heap[smallest]:
+            smallest = right_child_index
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
+
+
+    def insert(self, value):
+
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+
+    def extract_min(self):
+
+        if len(self.heap) == 0:
+            return None
+
+        min_value = self.heap[0]
+        last_value = self.heap.pop()
+        
+        if len(self.heap) > 0:
+            self.heap[0] = last_value
+            self._heapify_down(0)
+        
+        return min_value
+
+
+    def find_max(self):
+
+        """
+        I know is an overkill to look up for maximums like this but is a halfway between actually calling a max() to the list on which the heap is built
+        and to iterate over each parent-child groups, just for sake of learning and coding.
+        
+        everything within a while loop that is going to break when the comparison pass the last leaf node
+        1. Make ranges to pull up the first group of childs and find the minium and store it.
+        2. Refresh the range for the next level of childs, up to the total amount of childs or the end of the heap.
+        """
+
+        if len(self.heap) == 0:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap[0]             
+       
+        childs_range = [1,2]
+        max_val = max([self.heap[i] for i in range(childs_range[0], childs_range[1]+1)])
+        index = int()
+
+        while index < len(self.heap):
+
+            # Taking all the child nodes from the next level
+            childs_range = [childs_range[0]*2+1, childs_range[1]*2+2]   
+
+            # If the new upper bound is greater than the heap, take the last index of the heap instead
+            if childs_range[1] > len(self.heap)-1:  
+                childs_range[1] = len(self.heap)-1
+
+            childs_max = max([self.heap[i] for i in range(childs_range[0], childs_range[1]+1)])
+
+            if childs_max > max_val:
+                max_val = childs_max
+
+            index = childs_range[-1] + 1
+
+        return max_val
+
+
+    def heap_len(self):
+        return len(self.heap)
+
+
+    def display(self):
+        print(self.heap)
+
+
+    def delete(self, index):
+
+        """
+        This method will handle there cases:
+        1. if the deletion is in the last leaf
+        2. if the deletion is in the root
+        3. if the deletion is anywhere else
+        """
+
+        ind = self._index_handler(index)
+
+        # Deletion at the last leaf
+        if ind == self.heap_len() - 1: 
+            self.pop()
+            return        
+
+        self.heap[ind] = self.heap[-1]
+        self.heap = self.heap[:-1]
+        
+        # Deletion at the root
+        if ind == 0:
+            self._heapify_down(0)
+            return
+        
+
+        # If the deletion is in between, after the replacement the heap need to validate up and down to maintain the MaxHeap
+        parent = ( ind - 1) // 2
+
+        if self.heap[ind] < self.heap[parent]:
+            self._heapify_up(ind)
+
+        else:
+            self._heapify_down(ind)
+      
 
     def pop(self):
         
@@ -171,64 +396,58 @@ class MaxHeap:
 
 
     def display_heap(self):
-
+        
         heap = self.heap
+        if not heap:
+            return
 
         def get_level(index):
             return (index.bit_length() - 1) if index > 0 else 0
 
+        def get_children_indices(parent_index):
+            left_child_index = 2 * parent_index + 1
+            right_child_index = 2 * parent_index + 2
+            return left_child_index, right_child_index
+
         def print_spaces(count):
             print(" " * count, end="")
 
-        def print_branches(levels, current_level, index):
-            if current_level >= levels:
-                return
-
-            elements_in_level = 2 ** current_level
-            spaces_between_elements = 2 ** (levels - current_level) - 1
-
-            print_spaces(spaces_between_elements)
-
-            for i in range(elements_in_level):
-                current_index = (2 ** current_level - 1) + i
-                if current_index < len(heap):
-                    print(f"{heap[current_index]:>4}", end="")
-                else:
-                    break
-
-                if i < elements_in_level - 1:
-                    print_spaces(spaces_between_elements * 2)
-
-            print()
-
-            for i in range(elements_in_level):
-                current_index = (2 ** current_level - 1) + i
-                if current_index < len(heap):
-                    left_child_level = get_level(current_index * 2 + 1)
-                    right_child_level = get_level(current_index * 2 + 2)
-
-                    if left_child_level > current_level:
-                        print_spaces(spaces_between_elements)
-                    else:
-                        print_branches(levels, current_level + 1, current_index * 2)
-
-                    if right_child_level > current_level:
-                        print_spaces(spaces_between_elements)
-                    else:
-                        print_branches(levels, current_level + 1, current_index * 2 + 1)
-
-                    break
-
         height = get_level(len(heap) - 1) + 1
-        print_branches(height, 0, 0)
+        max_width = 2 ** height - 1
 
+        # Initialize queue with the root node
+        queue = [(0, 0)]  # (index, level)
 
+        current_level = 0
+        current_line_index = 0
 
+        while queue:
+            index, level = queue.pop(0)
 
+            if level > current_level:
+                # Move to the next line
+                print()
+                current_line_index = 0
+                current_level = level
 
+            if index < len(heap):
+                # Print the value
+                print_spaces(max_width // (2 ** (level + 1)))
+                print(f"{heap[index]:>4}", end="")
+            else:
+                # Print spaces for missing elements
+                print_spaces(max_width // (2 ** (level + 1) + 1))
 
+            current_line_index += 1
 
-class MinHeap: pass
+            # Add children to the queue
+            left_child_index, right_child_index = get_children_indices(index)
+            if left_child_index < len(heap):
+                queue.append((left_child_index, level + 1))
+            if right_child_index < len(heap):
+                queue.append((right_child_index, level + 1))
+
+        print()  # Print a newline at the end
 
 
 
