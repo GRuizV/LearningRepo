b71d34edcfe03b39a5ceae203d76236ae33095bf
Gerardo Ruiz <grville93@gmail.com>
2023-08-16 17:03:35 -0500

    No comments

--- Data Structures/Heaps, Stacks and Queues/Stacks/Stack_LifoQueue_Testing.py
+++ Data Structures/Heaps, Stacks and Queues/Stacks/Stack_LifoQueue_Testing.py
@@ -1,29 +1,30 @@
 import queue
 import random
 
-stack = queue.LifoQueue(maxsize=5)
+
+'LifoQueue Implementation'
+
+# stack = queue.LifoQueue(maxsize=5)
+
+# input_data = [random.randint(0,30) for _ in range(5)]
+# [stack.put(num) for num in input_data]
 
 
-input_data = [random.randint(0,30) for _ in range(5)]
-[stack.put(num) for num in input_data]
+# print(f'Stack maxsize: {stack.maxsize}')
+
+# print(f'Empty Stack: {stack.empty()}')
+
+# print(f'Full Stack: {stack.full()}')
+
+# print(f'Stack Size: {stack.qsize()}')
+
+# print(stack.queue)
+
+# item = stack.get()
+
+# print(stack.queue, f'removed: {item}')
+
+# print(f'Stack Size: {stack.qsize()}')
 
 
-print(f'Stack maxsize: {stack.maxsize}')
 
-print(f'Empty Stack: {stack.empty()}')
-
-print(f'Full Stack: {stack.full()}')
-
-print(f'Stack Size: {stack.qsize()}')
-
-print(stack.queue)
-
-item = stack.get()
-
-print(stack.queue, f'removed: {item}')
-
-print(f'Stack Size: {stack.qsize()}')
-print()
-
-
-print(stack.all_tasks_done)
