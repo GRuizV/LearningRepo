3910ba16f73b7b46a3f4ae290078f41cbb229100
Gerardo Ruiz <grville93@gmail.com>
2023-08-22 18:43:41 -0500

    Regular Queues  implemented and tested

--- Data Structures/Heaps, Stacks and Queues/Queues/Queues_Implementation.py
+++ Data Structures/Heaps, Stacks and Queues/Queues/Queues_Implementation.py
@@ -1,5 +1,61 @@
 
-class MyQueue: pass
+class MyQueue: 
+
+    def __init__(self, size):
+        
+        self.size = size
+        self.queue = [None] * size
+        self.front = self.rear = -1
+
+
+    def is_empty(self):
+
+        return self.front == -1
+
+
+    def is_full(self):
+
+        return self.rear == self.size - 1
+
+
+    def enqueue(self, data):
+
+        if self.is_full():
+            return print('Full Queue, Cannot Enqueue')
+        
+        if self.is_empty():
+            self.rear = self.front = 0
+
+        else:
+            self.rear += 1
+               
+        self.queue[self.rear] = data
+
+
+    def dequeue(self):
+
+        if self.is_empty():
+            return print('Empty Queue, Cannot Dequeue')
+        
+        item = self.queue[self.front]
+        
+        if self.rear == self.front:
+            self.rear = self.front = -1
+
+        else:
+            self.rear -= 1
+
+        return item
+
+
+    def display (self):
+
+        if self.is_empty():
+           return print('Empty Queue')
+        
+        [print(elem, end=' ') for elem in self.queue[:self.rear]]
+
+        print(self.queue[self.rear])
 
 
 
--- Data Structures/Heaps, Stacks and Queues/Queues/Queues_Testing.py
+++ Data Structures/Heaps, Stacks and Queues/Queues/Queues_Testing.py
@@ -2,110 +2,187 @@ import random
 import Queues_Implementation
 
 
+'Regular Queues'
+
+# my_queue = Queues_Implementation.MyQueue(size = 5)
+# [my_queue.enqueue(x) for x in [1, 8, 3, 5, 2]]
+# my_queue.display()
+# print()
+
+
+# my_queue.enqueue('X')
+# print()
+
+# print(f'Queue:', end=' ')
+# my_queue.display()
+# print(f'Queue front: {my_queue.queue[my_queue.front]}')
+# print(f'Queue rear: {my_queue.queue[my_queue.rear]}')
+# print(f'Queue as att.: {my_queue.queue}')
+# print(f'Queue front index: {my_queue.front}')
+# print(f'Queue rear index: {my_queue.rear}')
+
+
+# my_queue.dequeue()
+# print()
+
+
+# print(f'Queue:', end=' ')
+# my_queue.display()
+# print(f'Queue front: {my_queue.queue[my_queue.front]}')
+# print(f'Queue rear: {my_queue.queue[my_queue.rear]}')
+# print(f'Queue as att.: {my_queue.queue}')
+# print(f'Queue front index: {my_queue.front}')
+# print(f'Queue rear index: {my_queue.rear}')
+
+
+# my_queue.enqueue('X')
+# print()
+
+
+# print(f'Queue:', end=' ')
+# my_queue.display()
+# print(f'Queue front: {my_queue.queue[my_queue.front]}')
+# print(f'Queue rear: {my_queue.queue[my_queue.rear]}')
+# print(f'Queue as att.: {my_queue.queue}')
+# print(f'Queue front index: {my_queue.front}')
+# print(f'Queue rear index: {my_queue.rear}')
+
+
+# my_queue.dequeue()
+# my_queue.dequeue()
+# my_queue.dequeue()
+# my_queue.dequeue()
+# print()
+
+
+# print(f'Queue:', end=' ')
+# my_queue.display()
+# print(f'Queue front: {my_queue.queue[my_queue.front]}')
+# print(f'Queue rear: {my_queue.queue[my_queue.rear]}')
+# print(f'Queue as att.: {my_queue.queue}')
+# print(f'Queue front index: {my_queue.front}')
+# print(f'Queue rear index: {my_queue.rear}')
+
+
+# my_queue.dequeue()
+# print()
+
+
+# print(f'Queue:', end=' ')
+# my_queue.display()
+# print(f'Queue front: {my_queue.queue[my_queue.front]}')
+# print(f'Queue rear: {my_queue.queue[my_queue.rear]}')
+# print(f'Queue as att.: {my_queue.queue}')
+# print(f'Queue front index: {my_queue.front}')
+# print(f'Queue rear index: {my_queue.rear}')
+
+
+
+
 'Circular Queue'
 
-cir_queue = Queues_Implementation.MyCircularQueue(size = 5)
+# cir_queue = Queues_Implementation.MyCircularQueue(size = 5)
 
-# [cir_queue.enqueue(random.randint(0,5)) for _ in range(5)]
-[cir_queue.enqueue(x) for x in [1, 8, 3, 5, 2]]
+# # [cir_queue.enqueue(random.randint(0,5)) for _ in range(5)]
+# [cir_queue.enqueue(x) for x in [1, 8, 3, 5, 2]]
+# # cir_queue.display()
+# print()
+
+
+# print(f'Queue:', end=' ')
 # cir_queue.display()
-print()
-
-
-print(f'Queue:', end=' ')
-cir_queue.display()
-print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
-print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
-print(f'Queue as att.: {cir_queue.queue}')
-print(f'Queue front index: {cir_queue.front}')
-print(f'Queue rear index: {cir_queue.rear}')
+# print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
+# print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
+# print(f'Queue as att.: {cir_queue.queue}')
+# print(f'Queue front index: {cir_queue.front}')
+# print(f'Queue rear index: {cir_queue.rear}')
 
 
 
-cir_queue.dequeue()
-print()
+# cir_queue.dequeue()
+# print()
 
 
 
-print(f'Queue:', end=' ')
-cir_queue.display()
-print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
-print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
-print(f'Queue as att.: {cir_queue.queue}')
-print(f'Queue front index: {cir_queue.front}')
-print(f'Queue rear index: {cir_queue.rear}')
+# print(f'Queue:', end=' ')
+# cir_queue.display()
+# print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
+# print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
+# print(f'Queue as att.: {cir_queue.queue}')
+# print(f'Queue front index: {cir_queue.front}')
+# print(f'Queue rear index: {cir_queue.rear}')
 
 
 
-cir_queue.enqueue('X')
-print()
+# cir_queue.enqueue('X')
+# print()
 
 
 
-print(f'Queue:', end=' ')
-cir_queue.display()
-print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
-print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
-print(f'Queue as att.: {cir_queue.queue}')
-print(f'Queue front index: {cir_queue.front}')
-print(f'Queue rear index: {cir_queue.rear}')
+# print(f'Queue:', end=' ')
+# cir_queue.display()
+# print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
+# print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
+# print(f'Queue as att.: {cir_queue.queue}')
+# print(f'Queue front index: {cir_queue.front}')
+# print(f'Queue rear index: {cir_queue.rear}')
 
 
 
-cir_queue.enqueue('X')
-print()
+# cir_queue.enqueue('X')
+# print()
 
 
-cir_queue.dequeue()
-cir_queue.dequeue()
-cir_queue.dequeue()
-cir_queue.dequeue()
-print()
-
-
-
-print(f'Queue:', end=' ')
-cir_queue.display()
-print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
-print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
-print(f'Queue as att.: {cir_queue.queue}')
-print(f'Queue front index: {cir_queue.front}')
-print(f'Queue rear index: {cir_queue.rear}')
+# cir_queue.dequeue()
+# cir_queue.dequeue()
+# cir_queue.dequeue()
+# cir_queue.dequeue()
+# print()
 
 
 
-cir_queue.dequeue()
-print()
+# print(f'Queue:', end=' ')
+# cir_queue.display()
+# print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
+# print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
+# print(f'Queue as att.: {cir_queue.queue}')
+# print(f'Queue front index: {cir_queue.front}')
+# print(f'Queue rear index: {cir_queue.rear}')
 
 
 
-print(f'Queue:', end=' ')
-cir_queue.display()
-print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
-print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
-print(f'Queue as att.: {cir_queue.queue}')
-print(f'Queue front index: {cir_queue.front}')
-print(f'Queue rear index: {cir_queue.rear}')
+# cir_queue.dequeue()
+# print()
 
 
 
-[cir_queue.enqueue(x) for x in [1, 8, 3, 5, 2]]
-print()
+# print(f'Queue:', end=' ')
+# cir_queue.display()
+# print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
+# print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
+# print(f'Queue as att.: {cir_queue.queue}')
+# print(f'Queue front index: {cir_queue.front}')
+# print(f'Queue rear index: {cir_queue.rear}')
 
 
 
-cir_queue.enqueue('X')
-print()
+# [cir_queue.enqueue(x) for x in [1, 8, 3, 5, 2]]
+# print()
 
 
 
-print(f'Queue:', end=' ')
-cir_queue.display()
-print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
-print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
-print(f'Queue as att.: {cir_queue.queue}')
-print(f'Queue front index: {cir_queue.front}')
-print(f'Queue rear index: {cir_queue.rear}')
+# cir_queue.enqueue('X')
+# print()
+
+
+
+# print(f'Queue:', end=' ')
+# cir_queue.display()
+# print(f'Queue front: {cir_queue.queue[cir_queue.front]}')
+# print(f'Queue rear: {cir_queue.queue[cir_queue.rear]}')
+# print(f'Queue as att.: {cir_queue.queue}')
+# print(f'Queue front index: {cir_queue.front}')
+# print(f'Queue rear index: {cir_queue.rear}')
 
 
 
