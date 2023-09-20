063f6bfe53606545048e67612a4c2642df7dfa00
Gerardo Ruiz <grville93@gmail.com>
2023-09-19 19:56:20 -0500

    Stacks and Queues - Largest Rectangle solution made / Testing the optimal solution still pending

--- Python/HackerRank/Interview Preparation Kit/Interview Preparation Kit Exercises.py
+++ Python/HackerRank/Interview Preparation Kit/Interview Preparation Kit Exercises.py
@@ -71,6 +71,109 @@ from collections import deque, Counter
 
 
 
-'Queues: A Tale of Two Stacks'
+'Stacks and Queues - Largest Rectangle'
 
-'-Yet to be solved-'
+''' 
+First, I'm gonna try by brute force since it isn't evidently solvable with something I already know,
+later I will try to find an optimal solution
+
+'''
+
+# buildings = [1, 2, 3, 4, 5, 1, 2, 4, 6]
+# buildings = [1, 2, 3, 4, 5]
+# buildings = [11,11,10,10,10]
+buildings = [2,1,3,4,2,1,3,2,4]
+
+rectangles = list()
+
+
+# 1st approach
+# for i in range(len(buildings)):
+
+#     adj_buildings = list()
+
+#     for j in range(i, len(buildings) ):
+
+#         if buildings[j] < buildings[i]:
+#             break
+
+#         adj_buildings.append(buildings[j])
+
+#     if j == len(buildings) - 1:
+#         j = len(buildings)
+
+#     rectangles.append( min(adj_buildings) *  len(buildings[i:j])  ) 
+
+
+
+''' 
+This second approach actually worked, but it has nothing to do with stacks nor Queues, and has a complexity of O(n)^2
+I will research to understand the intention of the exercise and a optimal solution
+'''
+
+# # 2nd approach
+# for i in range(len(buildings)):
+
+#     adj_buildings = list()
+
+#     for j in range(len(buildings[:i]),-1,-1):
+
+#         if buildings[j] < buildings[i]:
+#             break
+
+#         adj_buildings.append(buildings[j])
+
+
+#     for j in range(i+1, len(buildings)):
+
+#         if buildings[j] < buildings[i]:
+#             break
+
+#         adj_buildings.append(buildings[j])
+
+
+#     num_buildings = len(adj_buildings)
+#     rectangle = num_buildings * buildings[i]
+#     rectangles.append(rectangle)
+    
+
+# print(rectangles)
+
+# print( max (rectangles) )
+
+
+
+
+''' 
+Finally the solution with stacks is below
+    But, the logic behind it actually eludes me :/
+'''
+
+h = [2,1,3,4,2,1,3,2,4]
+
+max_area = 0
+stack = list()
+
+for i, current_height in enumerate(h):
+
+    start_position = i
+
+    while stack and (stack[-1][1] > current_height):
+
+        index, height = stack.pop()
+
+        current_area = (i - index) * height
+
+        max_area = max(max_area, current_area)
+
+        start_position = index
+
+    stack.append((start_position, current_height))
+
+for i, current_height in stack:
+
+    current_area = current_height * (len(h) - i)
+
+    max_area = max(max_area, current_area)
+
+print (max_area)
