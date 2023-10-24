43d392ed314323c29259b20a47c76622d056df75
Gerardo Ruiz <grville93@gmail.com>
2023-10-24 12:29:51 -0500

    -Ema's Supercomputer- Challenge with progress, at 34% Efficacy, need a plus overlap check aux function to finish

--- HackerRank/Problem Solving/Problem Solving Excercises - pt. III.py
+++ HackerRank/Problem Solving/Problem Solving Excercises - pt. III.py
@@ -15,82 +15,157 @@ import random
 # BGBBGB
 # BGBBGB'''
 
-# grid = [x for x in a.split('\n')]
+# a ='''GGBBGBBGBBG
+# GGBBGBBGBBG
+# GGGGGGGGGGG
+# GGBBGBBGBBG
+# GGGGGGGGGGG
+# GGBBGBBGBBG
+# GGBBGBBGBBG
+# GGGGGGGGGGG
+# GGBBGBBGBBG
+# GGGGGGGGGGG
+# GGBBGBBGBBG
+# GGBBGBBGBBG
+# GGBBGBBGBBG'''
+
+a='''GBBBBBBGGGBGGBB
+GBBBBBBGGGBGGBB
+GBBBBBBGGGBGGBB
+GBBBBBBGGGBGGBB
+GGGGGGGGGGGGGGG
+GGGGGGGGGGGGGGG
+GBBBBBBGGGBGGBB
+GBBBBBBGGGBGGBB
+GGGGGGGGGGGGGGG
+GBBBBBBGGGBGGBB
+GBBBBBBGGGBGGBB
+GGGGGGGGGGGGGGG
+GGGGGGGGGGGGGGG
+GBBBBBBGGGBGGBB'''
+
+grid = [x for x in a.split('\n')]
 
 
-grid = ['BGBBBBB','GGGBBBB','BGBBGBB','BBBBGBB','BBGGGGG','BBBBGBB','BBBBGBB']
+# grid = ['BGBBBBB','GGGBBBB','BGBBGBB','BBBBGBB','BBGGGGG','BBBBGBB','BBBBGBB']
+# grid = ['BBBGBBBBBB','BBBGBBBBBB','BBBGBBBBBB','GGGGGGGBBB','BBBGBBGBBB','BBBGBBGBBB','BBBGGGGGGB','BBBBBBGBBB','BBBBBBGBBB','BBBBBBBBBB']
 
-for i in grid: print(i)
-print()
+
+# for i in grid: print(i)
+# print()
+
 
 
 
 
 # My solution
 
-'Auxiliary Function'
-# # a plus argument comes in the form "[Plus' area, (i, j)]"    being i,j the coordinates
-# def plus_checker(plus):
+'1st Auxiliary Function'
+# a plus argument comes in the form "[Plus' area, (i, j)]"    being i,j the coordinates
+def plus_checker(plus):
 
-#     if plus:
+    if plus:
 
-#         try:
-#             area = plus[0]
-#             up = plus[1][0]
-#             down = plus[1][1]
-#             left = plus[1][2]
-#             right = plus[1][3]
+        try:
+            area = plus[0]
+            up = plus[1][0]
+            down = plus[1][1]
+            left = plus[1][2]
+            right = plus[1][3]
 
-#             if 'B' not in ( grid[up[0]-1][up[1]], grid[down[0]+1][down[1]], grid[left[0]][left[1]-1], grid[right[0]][right[1]+1] ):
+            group = ( grid[up[0]-1][up[1]], grid[down[0]+1][down[1]], grid[left[0]][left[1]-1], grid[right[0]][right[1]+1] )
 
-#                 return [ area + 1, ( (up[0]-1, up[1]), (down[0]+1, down[1]), (left[0], left[1]-1), (right[0],right[1]+1) ) ]
+            within_limits = [up[0]>0, left[1]>0, down[0]<len(grid), right[1]<len(grid[0])]
 
-#         except IndexError:
+            if all(within_limits) and 'B' not in group:
+                return [ area + 4,  ((up[0]-1, up[1]),  (down[0]+1, down[1]), (left[0], left[1]-1), (right[0], right[1]+1)) ] 
 
-#             return        
 
+        except IndexError:
+            return        
+
+
+
+'2nd Auxiliary Function'
+# a plus argument comes in the form "[Plus' area, (i, j)]"    being i,j the coordinates
+def plus_overlap_check(pls1, pls2):
+
+    if True:
+        return True
     
+    else:
+        return False
+
+
+
 
 'Body of the main function'
-# possible_p = list()
 
-# for row in range(1, len(grid[1:-1])+1):
+possible_p = list()
+
+for row in range(1, len(grid[1:-1])+1):
     
-#     for j in range(1, len(grid[row][1:-1])+1):
+    for j in range(1, len(grid[row][1:-1])+1):
 
-#         if 'B' not in (grid[row-1][j], grid[row+1][j], grid[row][j-1], grid[row][j+1]):
+        if 'B' not in (grid[row-1][j], grid[row+1][j], grid[row][j-1], grid[row][j+1]):
 
-#             # Stored as "[Plus' area, (the four ends of the plus)]"
-#             possible_p.append([ 5, ( (row-1,j), (row+1,j), (row,j-1), (row,j+1) ) ])
-
-
-
-# # [5, ((0, 1), (2, 1), (1, 0), (1, 2))]
-# # ...
-# print(possible_p)
-# print()
-
-
-# for i in range(len(possible_p)):
-
-#     plus = possible_p[i]
-
-#     while plus is not None:
-
-#         plus = plus_checker(plus)
-
-#         if plus is not None:
-
-#             possible_p[i] = plus
+            # Stored as "[Plus' area, (the four ends of the plus)]"
+            possible_p.append([ 5, ( (row-1,j), (row+1,j), (row,j-1), (row,j+1) ) ])
 
 
 
 
+for i in possible_p : print(i)
+print()
 
-# possible_p.sort(key = lambda x: x[0], reverse=True)
 
 
-# print(possible_p)
+for i in range(len(possible_p)):
+
+    plus = possible_p[i]
+
+    while plus is not None:
+
+        plus = plus_checker(plus)
+
+        if plus is not None:
+
+            possible_p[i] = plus
+
+
+possible_p.sort(key= lambda x: x[0], reverse=True)
+
+for i in possible_p : print(i)
+print()
+
+
+candidates = possible_p
+possible_p = list(set([x[0] for x in possible_p]))
+possible_p.sort(reverse=True)
+
+for i in possible_p : print(i)
+print()
+
+if len(possible_p) == 0:
+    print( 0 )
+
+elif len(candidates) == 1:
+    print( possible_p[0] )
+
+else:
+    
+    if len(possible_p) == 1:
+        print( possible_p[0]*possible_p[0] )
+
+    else:
+        print( possible_p[0]*possible_p[1] )
+
+
+
+
+
+
+
 
 
 
@@ -127,78 +202,87 @@ print()
 
 
 
+
+
+
 'HackerRank Output'
 
-def plus_checker(plus):
+# def plus_checker(plus):
 
-    if plus:
+#     if plus:
 
-        try:
-            area = plus[0]
-            up = plus[1][0]
-            down = plus[1][1]
-            left = plus[1][2]
-            right = plus[1][3]
+#         try:
+#             area = plus[0]
+#             up = plus[1][0]
+#             down = plus[1][1]
+#             left = plus[1][2]
+#             right = plus[1][3]
 
-            group = ( grid[up[0]-1][up[1]], grid[down[0]+1][down[1]], grid[left[0]][left[1]-1], grid[right[0]][right[1]+1] )
+#             group = ( grid[up[0]-1][up[1]], grid[down[0]+1][down[1]], grid[left[0]][left[1]-1], grid[right[0]][right[1]+1] )
 
-            within_limits = [up[0]>0, left[1]>0, down[0]<len(grid), right[1]<len(grid[0])]
+#             within_limits = [up[0]>0, left[1]>0, down[0]<len(grid), right[1]<len(grid[0])]
 
-            if all(within_limits) and 'B' not in group:
-                return [ area + 1,  ((up[0]-1, up[1]),  (down[0]+1, down[1]), (left[0], left[1]-1), (right[0], right[1]+1)) ] 
+#             if all(within_limits) and 'B' not in group:
+#                 return [ area + 4,  ((up[0]-1, up[1]),  (down[0]+1, down[1]), (left[0], left[1]-1), (right[0], right[1]+1)) ] 
 
 
-        except IndexError:
-            return  
+#         except IndexError:
+#             return  
         
 
-def twoPluses(grid):
+# def twoPluses(grid):
 
-    possible_p = list()
+#     possible_p = list()
 
-    for row in range(1, len(grid[1:-1])+1):
+#     for row in range(1, len(grid[1:-1])+1):
         
-        for j in range(1, len(grid[row][1:-1])+1):
+#         for j in range(1, len(grid[row][1:-1])+1):
 
-            if 'B' not in (grid[row-1][j], grid[row+1][j], grid[row][j-1], grid[row][j+1]):
+#             if 'B' not in (grid[row-1][j], grid[row+1][j], grid[row][j-1], grid[row][j+1]):
 
-                # Stored as "[Plus' area, (the four ends of the plus)]"
-                possible_p.append([ 5, ( (row-1,j), (row+1,j), (row,j-1), (row,j+1) ) ])
-
-
-    for i in range(len(possible_p)):
-
-        plus = possible_p[i]
-
-        while plus is not None:
-
-            plus = plus_checker(plus)
-
-            if plus is not None:
-
-                possible_p[i] = plus
+#                 # Stored as "[Plus' area, (the four ends of the plus)]"
+#                 possible_p.append([ 5, ( (row-1,j), (row+1,j), (row,j-1), (row,j+1) ) ])
 
 
-    possible_p.sort(key = lambda x: x[0], reverse=True)
+#     for i in range(len(possible_p)):
+
+#         plus = possible_p[i]
+
+#         while plus is not None:
+
+#             plus = plus_checker(plus)
+
+#             if plus is not None:
+
+#                 possible_p[i] = plus
+
+#     candidates = possible_p
+#     possible_p = list(set([x[0] for x in possible_p]))
+#     possible_p.sort(reverse=True)
 
 
-    if len(possible_p) == 0:
-        return 0
+#     if len(possible_p) == 0:
+#         return 0
     
-    elif len(possible_p) == 1:
-        return possible_p[0][0]
-    
-    else:
-        return possible_p[0][0]*possible_p[1][0]
+#     elif len(candidates) == 1:
+#         return possible_p[0]
+        
+#     else:
+        
+#         if len(possible_p) == 1:
+#             return possible_p[0]*possible_p[0] 
+        
+#         else:
+#             return possible_p[0]*possible_p[1]
 
 
 
-print(twoPluses(grid))
+# print(twoPluses(grid))
 
 
 
 '''
-So far the code works, the only thing left is to check the pluses does not overlap each other
+My solution up to this point is 34% effective.
 
 '''
 
@@ -210,3 +294,5 @@ So far the code works, the only thing left is to check the pluses does not overl
 "xxx"
 
 # '-xxx- HackerRank Problem Solving Challenge done'
+
+
