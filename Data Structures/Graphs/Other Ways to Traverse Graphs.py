0b70c07d483938aadf6998c8d5398200064c013d
Gerardo Ruiz <grville93@gmail.com>
2023-10-30 21:54:50 -0500

    Topological Sort, Dijkstra & A* reviewed again and corrected

--- Data Structures/Graphs/Other Ways to Traverse Graphs.py
+++ Data Structures/Graphs/Other Ways to Traverse Graphs.py
@@ -5,34 +5,35 @@ import heapq
 
 'Topological Sort'
 
-# def topological_sort(graph):
+def topological_sort(graph):
 
-#     def topological_sort_util(node, visited, stack):
+    def topological_sort_util(node, visited, stack):
 
-#         visited[node] = True
+        visited[node] = True
 
-#         for neighbor in graph.get(node, []):
+        for neighbor in graph.get(node, []):
 
-#             if neighbor not in visited or not visited[neighbor]:
+            if neighbor not in visited or not visited[neighbor]:
 
-#                 topological_sort_util(neighbor, visited, stack)
+                topological_sort_util(neighbor, visited, stack)
 
-#         stack.insert(0, node)
+        stack.insert(0, node)
 
 
-#     visited = {node: False for node in graph}
-#     stack = list()
+    visited = {node: False for node in graph}
+    stack = list()
 
-#     for node in graph:
+    for node in graph:
         
-#         if not visited[node]:
+        if not visited[node]:
             
-#             topological_sort_util(node, visited, stack)
+            topological_sort_util(node, visited, stack)
 
-#     return ' '.join(str(x) for x in stack)
+    return ' '.join(str(x) for x in stack)
 
 
 
+'Topological Sort Testing'
 
 # # Graph initialization
 # al_graph = ALGraph()
@@ -62,34 +63,36 @@ import heapq
 
 "Dijkstra's Algorithm"
 
-# def dijkstra(graph, start_vertex):
+def dijkstra(graph, start_vertex):
 
-#     distances = {vertex: float('infinity') for vertex in graph}
-#     distances[start_vertex] = 0
-#     priority_queue = [(0, start_vertex)]
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start_vertex] = 0
+    priority_queue = [(0, start_vertex)]
 
-#     while priority_queue:
+    while priority_queue:
 
-#         current_distance, current_vertex = heapq.heappop(priority_queue)
+        current_distance, current_vertex = heapq.heappop(priority_queue)
 
-#         # Ignore outdated entries
-#         if current_distance > distances[current_vertex]:
-#             continue
+        # Ignore outdated entries
+        if current_distance > distances[current_vertex]:
+            continue
 
-#         if current_vertex in graph:
+        if current_vertex in graph:
 
-#             for neighbor, weight in graph[current_vertex]:
+            for neighbor, weight in graph[current_vertex]:
 
-#                 distance = current_distance + weight
+                distance = current_distance + weight
 
-#                 if distance < distances.get(neighbor, float('infinity')):
+                if distance < distances.get(neighbor, float('infinity')):
 
-#                     distances[neighbor] = distance
-#                     heapq.heappush(priority_queue, (distance, neighbor))
+                    distances[neighbor] = distance
+                    heapq.heappush(priority_queue, (distance, neighbor))
 
-#     return distances
+    return distances
 
 
+
+"Dijkstra's testing"
 
 # # Graph initialization
 # al_graph = ALGraph()
@@ -100,7 +103,6 @@ import heapq
 #     "B": [("C", 2),("D", 5)],
 #     "C": [("D", 1)],
     
-
 # }
 
 
@@ -138,27 +140,36 @@ def astar(graph, start, goal):
     heapq.heappush(open_set, (0, start))
 
     while open_set:
+
         # Get the node with the lowest f-score from the open set
         _, current = heapq.heappop(open_set)
 
         if current == goal:
+
             # Reconstruct the path if the goal is reached
             path = []
+
             while current in parent:
+
                 path.insert(0, current)
                 current = parent[current]
+
             path.insert(0, start)
+
             return path
+
 
         closed_set.add(current)
 
         for neighbor, cost in graph[current]:
+
             if neighbor in closed_set:
                 continue
 
             tentative_g_score = g_score[current] + cost
 
             if tentative_g_score < g_score[neighbor]:
+
                 # This is a better path to the neighbor
                 parent[neighbor] = current
                 g_score[neighbor] = tentative_g_score
@@ -173,60 +184,58 @@ def heuristic(node, goal):
     return 0  # For Dijkstra's algorithm, set it to 0
 
 
-# Graph initialization
-al_graph = ALGraph()
 
-al_graph.graph = {
+'A* Testing'
 
-    "A": [("B", 1),("C", 4)],
-    "B": [("C", 2),("D", 5)],
-    "C": [("D", 1)],
+# # Graph initialization
+# al_graph = ALGraph()
+
+# al_graph.graph = {
+
+#     "A": [("B", 1),("C", 4)],
+#     "B": [("C", 2),("D", 5)],
+#     "C": [("D", 1)],
     
 
-}
+# }
 
-al_graph.graph = {
-    'A': [('B', 1), ('C', 3)],
-    'B': [('D', 2)],
-    'C': [('D', 2)],
-    'D': [('E', 3)],
-    'E': []
-}
-
-
-
-graph = al_graph.get_graph()
-start = "A"
-goal = "E"
+# al_graph.graph = {
+#     'A': [('B', 1), ('C', 3)],
+#     'B': [('D', 2)],
+#     'C': [('D', 2)],
+#     'D': [('E', 3)],
+#     'E': []
+# }
 
 
-# print(al_graph)
 
-path = astar(graph, start, goal)
-print(path)
-print()
-
-total_cost = int()
+# graph = al_graph.get_graph()
+# start = "A"
+# goal = "E"
 
 
-for node in path[:-1]:
+# # print(al_graph)
 
-    print(graph[node])
+# path = astar(graph, start, goal)
+# cost = int()
 
-    if node == 'A':
-        print(graph[node].index('B'))
+# for i in range(len(path[:-1])):
 
-
-'''
-A* Search relatively understood, still needs it complex case and some fixing in pulling the total cost of the path
-
-'''
+#     node = graph[path[i]]
+#     idx = [x[0] for x in node].index(path[i+1])
+#     cost += node[idx][1]
 
 
-# if path:
-#     print("Shortest path:", path)
-#     print("Total cost:", sum(graph[node][1] for node in path[:-1]))
+# print(f'A* path: {' '.join(str(x) for x in path)}', end= '\n')
+# print(f'A* cost: {cost}')
+# print()
 
-# else:
-#     print("No path found.")
+
+# dijkstra_path = dijkstra(graph, start)
+# print(f"Dijkstra's result ({dijkstra_path})")
+
+
+
+
+
 
