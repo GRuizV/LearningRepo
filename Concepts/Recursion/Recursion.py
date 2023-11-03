a95fc8f8cace7756eade2f9ced23b0d923e51c7d
Gerardo Ruiz <grville93@gmail.com>
2023-11-02 22:10:52 -0500

    A third simple recursion case was reviewed

--- Concepts/Recursion/Recursion.py
+++ Concepts/Recursion/Recursion.py
@@ -7,17 +7,18 @@
 #     if n == 0: return 1
 #     return n * factorial(n-1)
 
-# Case with print statements
-def factorial (n):
 
-    if n == 0:
-        print(f'f(0) = 1')
-        return 1
+# # Case with print statements
+# def factorial (n):
+
+#     if n == 0:
+#         print(f'f(0) = 1')
+#         return 1
     
-    else:
-        result = n * factorial(n-1)
-        print(f'f({n}) = {n} * f({n-1}) = {result}')
-        return result
+#     else:
+#         result = n * factorial(n-1)
+#         print(f'f({n}) = {n} * f({n-1}) = {result}')
+#         return result
 
 
 # n = 5
@@ -84,78 +85,109 @@ def factorial (n):
 
 'Now, with the correct printouts'
 
-# Base algorithm
-def fibonacci(n):
+# # Base algorithm
+# def fibonacci(n):
 
-    if n == 0: return 0
-    if n == 1: return 1
+#     if n == 0: return 0
+#     if n == 1: return 1
     
-    return fibonacci(n-2) + fibonacci(n-1)
+#     return fibonacci(n-1) + fibonacci(n-2)
 
 
-def fib_printouts(n):
+# def fib_printouts(n):
     
-    for i in range(n+1):
+#     for i in range(n+1):
 
-        if i == 0:
-            print(f'F({i}) = {fibonacci(i)}')
+#         if i == 0:
+#             print(f'F({i}) = {fibonacci(i)}')
 
-        elif i == 1:          
-            print(f'F({i}) = {fibonacci(i)}')
+#         elif i == 1:          
+#             print(f'F({i}) = {fibonacci(i)}')
 
-        else:
-            print(f'F({i}) = {fibonacci(i)}')
+#         else:
+#             print(f'F({i}) = {fibonacci(i)}')
+
+# # n = 5
+# # fib_printouts(n)
+
+
+
+# # I think it'd be better to return just a list of Fib's seq. up to the result 
+# def fib_seq(n):
+    
+#     result = [0,1]
+
+#     # # This is fine but could be more elegant
+#     # for i in range(2, n+1):
+#     #     result.append(fibonacci(i))
+
+#     # return result
+
+#     # This is more elegant!
+#     [result.append(fibonacci(i)) for i in range(2, n+1)]
+#     return result
+
 
 # n = 5
-# fib_printouts(n)
+# fib_seq_utn = fib_seq(n)
 
-
-
-# I think it'd be better to return just a list of Fib's seq. up to the result 
-def fib_seq(n):
-    
-    result = [0,1]
-
-    # # This is fine but could be more elegant
-    # for i in range(2, n+1):
-    #     result.append(fibonacci(i))
-
-    # return result
-
-    # This is more elegant!
-    [result.append(fibonacci(i)) for i in range(2, n+1)]
-    return result
-
-
-n = 5
-fib_seq_utn = fib_seq(n)
-
-print(f"Fibonacci's sequence up to '{n}':")
-print(fib_seq_utn)
+# print(f"Fibonacci's sequence up to '{n}':")
+# print(fib_seq_utn)
 
 
 
 
+# "This is my definitive version of the Fibonacci's Seq generator based on recursion"
 
-def fib_seq(n):
+# def fib_seq(n):
 
-    def fibonacci(n):
+#     def fibonacci(n):
 
-        if n == 0: return 0
-        if n == 1: return 1
+#         if n == 0: return 0
+#         if n == 1: return 1
         
-        return fibonacci(n-2) + fibonacci(n-1)
+#         return fibonacci(n-1) + fibonacci(n-2)
     
-    result = [0,1]
+#     result = [0,1]
 
-    [result.append(fibonacci(i)) for i in range(2, n+1)]
-    return result
-
-
+#     [result.append(fibonacci(i)) for i in range(2, n+1)]
+#     return result
 
 
-
+# print(fib_seq(5))
 
 
 
 
+
+"Another simple case: sum up to something"
+
+# # Base case
+# def rec_sum(n):
+
+#     if n == 1:       
+#         return 1
+   
+#     return n + rec_sum(n-1)
+
+
+# n = 5
+# print(rec_sum(n))
+
+
+
+
+# # Case with printouts
+# def rec_sum(n):
+
+#     if n == 1:
+#         print(f'It starts with: 1')
+#         return 1
+    
+#     result = n + rec_sum(n-1)  
+#     print(f'{result-n} + {n} = {result}')
+#     return result
+
+
+# n = 5
+# rec_sum(n)
