a2826fe27ee51938cc1eb5170196ea0d32736675
Gerardo Ruiz <grville93@gmail.com>
2023-11-08 17:37:27 -0500

    Snippet Added: Tower of Hanoi Recursive Solution Added

--- Useful_Snipets.py
+++ Useful_Snipets.py
@@ -936,3 +936,26 @@ The first recursion problem I review:
 #         else:
 #             count += 1
 #             i += 1
+
+
+
+
+
+
+
+"A recursive solution for the Tower of Hanoi"
+
+
+# def hanoi(n, start, end):
+    
+#     if n == 1:
+#         pm(start, end)
+
+#     else:
+#         other = 6 - (start + end)
+#         hanoi (n-1, start, other)
+#         pm(start, end)
+#         hanoi(n-1, other, end)
+
+# def pm(start, end):
+#     print(f"from {start} to {end}")
