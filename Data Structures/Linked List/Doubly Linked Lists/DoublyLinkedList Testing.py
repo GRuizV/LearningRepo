760870dd407c1d0f7159ffcfc55757b1f7734d94
Gerardo Ruiz <grville93@gmail.com>
2023-11-09 12:56:46 -0500

    Doubly Linked List Custom Module Tested: Pass

--- Data Structures/Linked List/Doubly Linked Lists/DoublyLinkedList Testing.py
+++ Data Structures/Linked List/Doubly Linked Lists/DoublyLinkedList Testing.py
@@ -1,106 +1,92 @@
-from DoublyLinkedList import *
-
-new_linked_list = DoublyLinkedList()
+from gr_linked_lists import DoublyLinkedList, Node
 
 
-new_linked_list.append('Beth')
-new_linked_list.append('George')
-new_linked_list.prepend('Jimmy')
-new_linked_list.append('Beth')
-new_linked_list.append('Kathy')
-new_linked_list.append('Beth')
-new_linked_list.append('Ben')
-new_linked_list.prepend('Mike')
-new_linked_list.insert_after('Joe', 'George')
+'List creation test'
+# new_linked_list = DoublyLinkedList()
 
+# [new_linked_list.append(x) for x in ['Beth', 'George', 'Beth', 'Kathy', 'Beth', 'Ben']]
+# new_linked_list.prepend('Jimmy')
+# new_linked_list.prepend('Mike')
+# new_linked_list.insert_after('Joe', 'George')
+
+# new_linked_list.print_list()
+
+
+# # ['Mike', 'Jimmy', 'Beth', 'George', 'Joe', 'Beth', 'Kathy', 'Beth', 'Ben']
 # new_linked_list.search('Zeke')
 # new_linked_list.search('Beth')
 # new_linked_list.search('George')
 
-# new_linked_list.delete('Ben')
 
+# new_linked_list.delete('Ben')
 # new_linked_list.print_list()
 
-new_linked_list.reverse()
 
+# new_linked_list.reverse()
 # new_linked_list.print_list()
 
 
 # test_list = DoublyLinkedList()
 
-# test_list.append('A')
-# test_list.append('B')
-# test_list.append('C')
-# test_list.append('D')
-
-
+# [test_list.append(x) for x in 'ABCD']
 # new_linked_list.extend(test_list)
 
 # new_linked_list.print_list()
 
 # a = new_linked_list.return_node(-1)
-
-# print(a.next)
-
+# print(f"\nLast node: '{a.data}', Last node next: {a.next}")
 
 
+# [new_linked_list.delete(x) for x in 'ABCD']
 # popped_node = new_linked_list.pop()
 
 # new_linked_list.print_list()
 
-# print(popped_node.data)
-
+# print(f"\nPopped node: '{popped_node.data}'")
 
 
 # new_linked_list.bubble_sort()
-
 # new_linked_list.print_list()
 
 # new_linked_list.print_nodes()
+
+
 
 # new_linked_list.delete('Ben')
 # new_linked_list.print_list()
-# print()
 
 # new_linked_list.print_nodes()
 
 
 
-new_list = DoublyLinkedList()
-
-[new_list.append(x) for x in 'BCA']
-
-new_list.print_list()
-print()
-
-new_list.bubble_sort()
-
-new_list.print_list()
-print()
-
-new_list.print_nodes()
 
 
+'Sorting Test'
+# new_list = DoublyLinkedList()
 
+# [new_list.append(x) for x in 'BCA']
 
+# new_list.print_list()
+# print()
 
+# new_list.bubble_sort()
 
+# new_list.print_list()
+# print()
 
-_ = '-----------------------------------------------------------------------'
+# new_list.print_nodes()
 
 
 
 
 
 
-
-_ = 'DOUBLY LINKED LIST EXERCISE'
+'DOUBLY LINKED LIST EXERCISE'
 
 '''
 9. Write a Python program to create a doubly linked list and print nodes from current position to first node.
 Click me to see the sample solution
 '''
-
 
 # new_linked_list.print_list()
 # print()
@@ -109,17 +95,18 @@ Click me to see the sample solution
 # def print_from(index, linked_list = DoublyLinkedList()):
 
 #     index = linked_list._index_handler(index)
-
 #     node = linked_list.return_node(index)
 
 #     while node is not None:
 
 #         print(node.data, end = ' ')
-
 #         node = node.prev
 
 
-# print_from(-5, new_linked_list)
+# print_from(-3, new_linked_list)
+
+
+
 
 
 
