51e3e803be22f3adb684f2dbda4944284799de03
Gerardo Ruiz <grville93@gmail.com>
2023-11-09 12:03:52 -0500

    Singly Linked List Custom Module Tested: Pass

--- Data Structures/Linked List/Singly Linked Lists/SinglyLinkedList Testing.py
+++ Data Structures/Linked List/Singly Linked Lists/SinglyLinkedList Testing.py
@@ -1,70 +1,50 @@
-from SinglyLinkedList import *
+from gr_linked_lists import SinglyLinkedList, Node
 
 
+'List creation test'
 # new_linked_list = SinglyLinkedList()
 
 # new_linked_list.head = Node('Albert')
 
-# new_linked_list.append('Beth')
-
-# new_linked_list.append('George')
-
+# [new_linked_list.append(x) for x in ['Beth', 'George', 'Beth', 'Kathy', 'Beth', 'Ben']]
 # new_linked_list.prepend('Jimmy')
-
-# new_linked_list.append('Beth')
-
-# new_linked_list.append('Kathy')
-
-# new_linked_list.append('Beth')
-
-# new_linked_list.append('Ben')
-
 # new_linked_list.insert_after('Jerome', 'Ben')
-
 # new_linked_list.prepend('Kevin')
-
 # new_linked_list.append('John')
-
 # new_linked_list.delete('Kevin')
-
 # new_linked_list.insert_after('Claude', 'Kathy')
-
 # new_linked_list.delete('John')
 
-# new_linked_list.print_list()
-# print()
+# # new_linked_list.print_list()
+# # print()
 
+# new_linked_list.bubble_sort()
 
-# new_linked_list.bubble_sort_list()
-
-
-# new_linked_list.print_list()
-# print()
-
+# # new_linked_list.print_list()
+# # print()
 
 # new_linked_list.reverse_list()
 
-
 # new_linked_list.print_list()
+# print()
 
 
+
+
+'Sorting Test'
 # new_list = SinglyLinkedList()
-
 
 # [new_list.append(x) for x in 'CDBEA']
 
-
 # new_list.print_list()
-
 # new_list.bubble_sort()
-
 # new_list.print_list()
 
 
 
 
 
-_ = 'Interview Preparation Kit - Removing dupliates from a Linked List'
+'Interview Preparation Kit - Removing duplicates from a Linked List'
 
 
 # new_list = SinglyLinkedList()
@@ -106,23 +86,24 @@ _ = 'Interview Preparation Kit - Removing dupliates from a Linked List'
 
 
 
-_ = 'Interview Preparation Kit - Find Merge Point of Two Lists'
+'Interview Preparation Kit - Find Merge Point of Two Lists'
 
 
-
-a = SinglyLinkedList()
-[a.append(x) for x in [1,2,3]]
-
-a_l = a._list_items()
-
-b = SinglyLinkedList()
-[b.append(x) for x in [1,2,3]]
-b_l = b._list_items()
+# a = SinglyLinkedList()
+# [a.append(x) for x in [1,2,3]]
+# a_l = a._list_items()
 
 
-for i in range(len(a_l)):
+# b = SinglyLinkedList()
+# [b.append(x) for x in [0,2,3]]
+# b_l = b._list_items()
 
-    print(a_l[i] is b_l[i])
+
+# print()
+# for i in range(len(a_l)):
+
+#     print(f"is a[{i}]:'{a_l[i]}', b[{i}]:'{ b_l[i]}', Answer: {a_l[i] is b_l[i]}")
 
 
-print (a_l == b_l)
+# print('\nIdentity Test for the two lists')
+# print (f"list a:'{a_l}' is list b:'{b_l}', Answer: {a_l == b_l}")
