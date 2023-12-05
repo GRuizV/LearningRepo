92db5a36e132f3bfb26bd1a3703a078dbbdaa16c
Gerardo Ruiz <grville93@gmail.com>
2023-12-04 20:47:04 -0500

    Detect Floaring Point Number - HackerRank Challenge: Done

---
+++ Concepts/RegEx/RegEx Exercises.py
@@ -0,0 +1,36 @@
+import re
+
+'Detect Floaring Point Number'
+
+# pattern = re.compile(r'[\+-]?\d*\.\d+')
+
+
+# text= '''
+# 12.
+# 12.0
+# '''
+
+# cases = [x for x in text.split('\n')][1:-1]
+# print(cases)
+
+
+# for case in cases:
+
+#     result = pattern.search(case)
+
+#     print(result.group() == case) if result else print(False)
+
+# Done
+
+
+
+
+'xxx'
+
+
+
+
+
+
+
+
