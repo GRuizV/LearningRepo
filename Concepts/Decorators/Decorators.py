bcbeecca06a0d587dcfeb818860a47f01ba1a013
Gerardo Ruiz <grville93@gmail.com>
2023-12-11 23:30:39 -0500

    Patrick Loeber & Corey Schafer Decorator tutorials, reviewed

--- Concepts/Decorators/Decorators.py
+++ Concepts/Decorators/Decorators.py
@@ -1,5 +1,6 @@
 from timeit import default_timer as timer
 import time 
+import functools
 
 
 "First... Closures"
@@ -164,25 +165,107 @@ import time
 
 
 
-def print_message(message):
-    "Enclosong Function"
-    def message_sender():
-        "Nested Function"
-        print(message)
-
-    message_sender()
-
-print_message("Some random message")
 
 
+"Patrick Loeber Tutorial"
+
+# def start_end_decorator(func):
+
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+
+#         print('Start')
+#         result = func(*args, **kwargs)
+#         print('End')
+
+#         return result
+    
+#     return wrapper
+
+
+# @start_end_decorator
+# def add5(x):
+#     return x + 5
+
+
+# # result = add5(10)
+# # print(result)
+
+# print(help(add5))
+# print(add5.__name__)    # add5
+
+
+
+
+'   Class decorators'
+
+# class CountCalls:
+
+#     def __init__(self, func):
+#         self.func = func
+#         self.num_calls = 0
+
+#     def __call__(self, *args, **kwargs):
+#         self.num_calls += 1
+#         print(f'This have been executed {self.num_calls} times')
+#         return self.func(*args, **kwargs)
+
+
+# @CountCalls
+# def say_hello():
+#     print('Hello!')
+
+
+# say_hello()
+# say_hello()
 
 
 
 
 
 
+"Corey Scheafer Tutorial"
+
+
+# def my_logger(orig_func):
+
+#     import logging
+#     logging.basicConfig(filename = f'{orig_func.__name__}.log', level=logging.INFO)
+
+#     @functools.wraps(orig_func)
+#     def wrapper(*args, **kwargs):
+
+#         logging.info(f'Ran with args: {args}, and kwargs: {kwargs}')
+#         return orig_func(*args, **kwargs)
+
+#     return wrapper
+
+
+# def my_timer(orig_func):
+
+#     import time
+
+#     @functools.wraps(orig_func)
+#     def wrapper(*args, **kwargs):
+
+#         t1 = time.time()
+#         result = orig_func(*args, **kwargs)
+#         t2 = time.time() - t1
+
+#         print(f'{orig_func.__name__} ran in: {t2:.2f} sec')
+
+#         return result
+
+#     return wrapper
+
+
+# @my_logger
+# @my_timer
+# def display_info(name, age):
+
+#     time.sleep(1)
+#     print(f'display_info ran with arguments ({name}, {age})')
 
 
 
-
-
+# display_info('Tom', 22)
