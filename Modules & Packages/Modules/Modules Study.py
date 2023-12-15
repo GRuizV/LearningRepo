1149e62e6ade5e32bbf6ff0d2fadfd1f85d9bad7
Gerardo Ruiz <grville93@gmail.com>
2023-12-15 17:28:52 -0500

    Modules & Packages directory made. / Modules tutorials reviewed

---
+++ Modules & Packages/Modules/Modules Study.py
@@ -0,0 +1,204 @@
+import sys
+
+
+
+'Built-in modules in the builtin_modules_names varibale'
+
+# for mod_name in sys.builtin_module_names:
+    # print(mod_name) 
+
+# '''
+#     Printout:
+
+#         _abc,
+#         _ast   
+#         _bisect
+#         _blake2
+#         _codecs
+#         _codecs_cn
+#         _codecs_hk
+#         _codecs_iso2022
+#         _codecs_jp
+#         _codecs_kr
+#         _codecs_tw
+#         _collections
+#         _contextvars
+#         _csv
+#         _datetime
+#         _functools
+#         _heapq
+#         _imp
+#         _io
+#         _json
+#         _locale
+#         _lsprof
+#         _md5
+#         _multibytecodec
+#         _opcode
+#         _operator
+#         _pickle
+#         _random
+#         _sha1
+#         _sha2
+#         _sha3
+#         _signal
+#         _sre
+#         _stat
+#         _statistics
+#         _string
+#         _struct
+#         _symtable
+#         _thread
+#         _tokenize
+#         _tracemalloc
+#         _typing
+#         _warnings
+#         _weakref
+#         _winapi
+#         _xxinterpchannels
+#         _xxsubinterpreters
+
+#         array
+#         atexit
+#         audioop
+#         binascii
+#         builtins
+#         cmath
+#         errno
+#         faulthandler
+#         gc
+#         itertools
+#         marshal
+#         math
+#         mmap
+#         msvcrt
+#         nt
+#         sys
+#         time
+#         winreg
+#         xxsubtype
+#         zlib
+# '''
+
+
+
+
+
+
+
+'Directions of the other modules in the path varibale from the sys module'
+
+# for dir_name in sys.path:
+#     print(dir_name) 
+
+# '''
+#     printout:
+
+#         C:\Users\USUARIO\GR\Software Development\Learning\Modules
+#         C:\Users\USUARIO\GR\Software Development\GR Custom Modules\Custom DS Modules
+#         C:\Users\USUARIO\AppData\Local\Programs\Python\Python312\python312.zip      
+#         C:\Users\USUARIO\AppData\Local\Programs\Python\Python312\DLLs
+#         C:\Users\USUARIO\AppData\Local\Programs\Python\Python312\Lib
+#         C:\Users\USUARIO\AppData\Local\Programs\Python\Python312
+#         C:\Users\USUARIO\AppData\Local\Programs\Python\Python312\Lib\site-packages
+
+# '''
+
+
+
+
+
+
+"Executing in the prompt python and using the command help('modules'), it returned the available modules there are for this python execution"
+
+# '''
+#     Printout:
+
+#     __future__          _thread             glob                random
+#     __hello__           _threading_local    gr_binary_search_tree re
+#     __phello__          _tkinter            gr_graphs           reprlib
+#     _abc                _tokenize           gr_hash_table       rlcompleter
+#     _aix_support        _tracemalloc        gr_heaps            runpy
+#     _ast                _typing             gr_linked_lists     sched
+#     _asyncio            _uuid               gr_queues           secrets
+#     _bisect             _warnings           gr_stacks           select
+#     _blake2             _weakref            graphlib            selectors
+#     _bz2                _weakrefset         gzip                shelve
+#     _codecs             _winapi             hashlib             shlex
+#     _codecs_cn          _wmi                heapq               shutil
+#     _codecs_hk          _xxinterpchannels   hmac                signal
+#     _codecs_iso2022     _xxsubinterpreters  html                site
+#     _codecs_jp          _zoneinfo           http                smtplib
+#     _codecs_kr          abc                 idlelib             sndhdr
+#     _codecs_tw          aifc                imaplib             socket
+#     _collections        antigravity         imghdr              socketserver
+#     _collections_abc    argparse            importlib           sqlite3
+#     _compat_pickle      array               iniconfig           sre_compile
+#     _compression        ast                 inspect             sre_constants
+#     _contextvars        asyncio             io                  sre_parse
+#     _csv                atexit              ipaddress           ssl
+#     _ctypes             audioop             itertools           stat
+#     _ctypes_test        base64              json                statistics
+#     _datetime           bdb                 keyword             string
+#     _decimal            binascii            lib2to3             stringprep
+#     _elementtree        bisect              linecache           struct
+#     _functools          builtins            locale              subprocess
+#     _hashlib            bz2                 logging             sunau
+#     _heapq              cProfile            lzma                symtable
+#     _imp                calendar            mailbox             sys
+#     _io                 cgi                 mailcap             sysconfig
+#     _json               cgitb               marshal             tabnanny
+#     _locale             chunk               math                tarfile
+#     _lsprof             cmath               mimetypes           telnetlib
+#     _lzma               cmd                 mmap                tempfile
+#     _markupbase         code                modulefinder        test
+#     _md5                codecs              msilib              textwrap
+#     _msi                codeop              msvcrt              this
+#     _multibytecodec     collections         multiprocessing     threading
+#     _multiprocessing    colorama            netrc               time
+#     _opcode             colorsys            nntplib             timeit
+#     _operator           compileall          nt                  tkinter
+#     _osx_support        concurrent          ntpath              token
+#     _overlapped         configparser        nturl2path          tokenize
+#     _pickle             contextlib          numbers             tomllib
+#     _py_abc             contextvars         opcode              trace
+#     _pydatetime         copy                operator            traceback
+#     _pydecimal          copyreg             optparse            tracemalloc
+#     _pyio               crypt               os                  tty
+#     _pylong             csv                 packaging           turtle
+#     _pytest             ctypes              pathlib             turtledemo
+#     _queue              curses              pdb                 types
+#     _random             dataclasses         pickle              typing
+#     _sha1               datetime            pickletools         unicodedata
+#     _sha2               dbm                 pip                 unittest
+#     _sha3               decimal             pipes               urllib
+#     _signal             difflib             pkgutil             uu
+#     _sitebuiltins       dis                 platform            uuid
+#     _socket             doctest             plistlib            venv
+#     _sqlite3            email               pluggy              warnings
+#     _sre                encodings           poplib              wave
+#     _ssl                ensurepip           posixpath           weakref
+#     _stat               enum                pprint              webbrowser
+#     _statistics         errno               profile             winreg
+#     _string             faulthandler        pstats              winsound
+#     _strptime           filecmp             pty                 wsgiref
+#     _struct             fileinput           py                  xdrlib
+#     _symtable           fnmatch             py_compile          xml
+#     _testbuffer         fractions           pyclbr              xmlrpc
+#     _testcapi           ftplib              pydoc               xxsubtype
+#     _testclinic         functools           pydoc_data          zipapp
+#     _testconsole        gc                  pyexpat             zipfile
+#     _testimportmultiple genericpath         pympler             zipimport
+#     _testinternalcapi   getopt              pytest              zlib
+#     _testmultiphase     getpass             queue               zoneinfo
+#     _testsinglephase    gettext             quopri
+
+# '''
+
+
+
+
+
+
+
+
