e94263ce96026675f0d8112d6d9fe4cf6408f874
Gerardo Ruiz <grville93@gmail.com>
2024-01-22 13:44:55 -0500

    The Exercise from Turing that Juan had to face, reviewed

--- Job Searching/Turing/Turing Challenges.py
+++ Job Searching/Turing/Turing Challenges.py
@@ -63,10 +63,55 @@
 
 
 
+'''
+This exercise is the one that Juan had to solve
+
+    Given an array of integers, always woth a pair length, the task is to return the max of the sum
+    of the first elements of all possible pairings within the array.
+'''
+
+# import itertools
+
+# # Input
+# s = [2,3,4,1]
+
+
+# # All possible pair combinations
+
+# pairs = list(itertools.combinations(s,2))   #[(2, 3), (2, 4), (2, 1), (3, 4), (3, 1), (4, 1)]
+# # print(pairs)
 
 
 
+# # Making all the possible combinations without repetition
+# comb = []
+
+# for i in range(len(s)-1):
+    
+#     for k in range(i, len(pairs)):
+
+#         if pairs[i][0] not in pairs[k] and pairs[i][1] not in pairs[k]:
+
+#             comb.append([pairs[i], pairs[k]])
+ 
+
+# #Verifying
+# print(comb) # [[(2, 3), (4, 1)], [(2, 4), (3, 1)], [(2, 1), (3, 4)]]
 
 
 
+# # Pulling the first value of each pair to sum them
+# result = []
 
+# for elem in comb:
+
+#     temp = []
+
+#     for pair in elem:
+#        temp.append(min(pair))
+    
+#     result.append(sum(temp))
+
+
+# # The result should be the max of that
+# print(max(result))
