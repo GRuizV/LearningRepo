7bbe9a59743654a0e4ee129bbca8a315e691f818
Gerardo Ruiz <grville93@gmail.com>
2024-01-31 17:29:58 -0500

    Longest Substring Without Repeating Characters Leetcode Challenge done

--- Exercises/LeetCode/leetCode Exercises - Pt. I.py
+++ Exercises/LeetCode/leetCode Exercises - Pt. I.py
@@ -165,4 +165,63 @@
 
 
 
-'xxx'
+'3. Longest Substring Without Repeating Characters'
+
+
+s = "abcabcbb"
+
+
+# # My solution
+# substrings = []
+
+# i = 0
+
+# while i < len(s):
+
+#     sub = str()
+
+#     for char in s[i:]:
+
+#         if char in sub:
+#             substrings.append(sub)
+#             break
+
+#         sub += char
+    
+#     if sub not in substrings:
+#         substrings.append(sub)
+
+#     i += 1
+
+# # print(substrings)
+
+# max_sub = max(substrings, key = len) if substrings else 0
+
+# # print(max_sub)
+
+# print(max_sub, len(max_sub))
+
+
+# Another more efficient solution
+
+def lengthOfLongestSubstring(s: str) -> int:
+        
+        n = len(s)
+        maxLength = 0
+        charMap = {}
+        left = 0
+        
+        for right in range(n):
+
+            if s[right] not in charMap or charMap[s[right]] < left:
+                charMap[s[right]] = right
+                maxLength = max(maxLength, right - left + 1)
+
+            else:
+                left = charMap[s[right]] + 1
+                charMap[s[right]] = right
+        
+        return maxLength
+
+
+lengthOfLongestSubstring(s)
