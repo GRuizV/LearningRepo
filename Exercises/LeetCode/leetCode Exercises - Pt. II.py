0e7e6652ef85816f26d90ae19fd04eff231927ad
Gerardo Ruiz <grville93@gmail.com>
2024-02-27 18:58:12 -0500

    23. Merge k Sorted Lists LeetCode Challenge Done

--- Exercises/LeetCode/leetCode Exercises - Pt. II.py
+++ Exercises/LeetCode/leetCode Exercises - Pt. II.py
@@ -51,7 +51,6 @@
 
 '21. Merge Two Sorted Lists'
 
-
 # Base
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
@@ -208,4 +207,82 @@ Note:
 
 
 
-'xxx'
+'23. Merge k Sorted Lists'
+
+# Base
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# Input
+
+# # 1st Input
+# #List 1
+# one1, two1, three1 = ListNode(1), ListNode(4), ListNode(5)
+# one1.next, two1.next = two1, three1
+
+# #List 2
+# one2, two2, three2 = ListNode(1), ListNode(3), ListNode(4)
+# one2.next, two2.next = two2, three2
+
+# #List 3
+# one3, two3 = ListNode(2), ListNode(6)
+# one3.next = two3
+
+# # List of lists
+# li = [one1, one2, one3]
+
+# My Approach
+
+'''
+Rationale:
+  
+    1. Create an empty node.
+    2. Assign the node with the minimum value as next
+    3. Move that node to its next node until reaches 'None'.
+    4. When every value within the input list is None, breakout the loop and return.
+'''
+
+# def mergeKLists(lists:list[ListNode]) -> ListNode:
+    
+#     lists = [x for x in lists if x.val != '']
+
+#     if len(lists) == 0:
+#         return ListNode('')
+
+
+#     head = ListNode('')
+#     curr = head
+#     li = lists
+
+#     while True:
+
+#         if li == [None]:
+#             break
+
+#         # Create a list of the current nodes in input that aren't None and sort them ascendingly by value
+#         li = sorted([node for node in li if node != None], key = lambda x: x.val)
+
+#         # Make the 'next_node' the next node to the curr None & move over to that node right away
+#         curr.next = li[0]
+#         curr = curr.next
+
+#         # Move over to the next node of next_node
+#         li[0] = li[0].next
+
+#     return head.next
+
+
+# res = mergeKLists([ListNode('')])
+# res_li = []
+
+# print(res)
+
+'Notes: It worked'
+
+
+
+
+'23. Merge k Sorted Lists'
