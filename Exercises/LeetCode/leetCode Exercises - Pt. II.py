9548ecb2b034c41c446f1d14c331ed1c9938f0d6
Gerardo Ruiz <grville93@gmail.com>
2024-03-07 20:12:09 -0500

    42. Trapping Rain Water Leetcode Challenge done

--- Exercises/LeetCode/leetCode Exercises - Pt. II.py
+++ Exercises/LeetCode/leetCode Exercises - Pt. II.py
@@ -642,4 +642,70 @@ Notes:
 
 
 
+'42. Trapping Rain Water'
+
+# Input
+
+# case 1
+# height = [0,1,0,2,1,0,1,3,2,1,2,1]  # Exp. Out: 6
+
+# case 2
+# height = [4,2,0,3,2,5]  # Exp. Out: 9
+
+
+# Solution
+
+# def trap(height):
+
+#     if not height:
+#         return 0
+    
+
+#     left, right = 0, len(height)-1
+#     left_max, right_max = 0, 0
+#     result = 0
+
+#     while left < right:
+
+#         if height[left] < height[right]:
+
+#             if height[left] >= left_max:
+#                 left_max = height[left]
+
+#             else:
+#                 result += left_max - height[left]
+
+#             left += 1
+        
+#         else:
+
+#             if height[right] >= right_max:
+#                 right_max = height[right]
+
+#             else:
+#                 result += right_max - height[right]
+
+#             right -= 1
+    
+#     return result
+
+
+# print(trap([3,0,2]))
+
+
+
+
+
 'xxx'
+
+
+
+
+
+
+
+
+
+
+
+
