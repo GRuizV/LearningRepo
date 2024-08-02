bca2c33c64c9326651c0ecbb811565546b82723d
Gerardo Ruiz <grville93@gmail.com>
2024-08-01 19:20:56 -0500

    Directories restructuring / Databases added: MySQL test.session file added

---
+++ Databases/MySQL/test.session.sql
@@ -0,0 +1,75 @@
+-- Comments in this files are preceeded by double hyphen '--'
+
+-- The '@block' marker indicates that the code below is an individual block of code to be executed individually
+
+
+
+
+-- From the 'The Fireship' Tutorial
+
+
+-- @block // Initial commands
+CREATE TABLE Users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    bio TEXT,
+    country VARCHAR(2)
+);
+
+
+-- @block // Individual entry insert
+INSERT INTO Users(email, bio, country)
+VALUES(
+    'hello@world.com',
+    'I love strangers!',
+    'US'
+)
+;-- Returned an error: 'Column count doesn't match value count at row 1' when country = 'USA'
+
+
+-- @block // Multiple entries insert
+INSERT INTO Users(email, bio, country)
+VALUES
+('hello@world.com', 'foo', 'US'),
+('hola@mundo.com', 'bar', 'MX'),
+('bnjour@monde.com', 'baz', 'FR') 
+
+
+-- @block
+SELECT * FROM Users;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- My Queries
+
+-- @block // Correcting the mistype of the column 'emai'
+ALTER TABLE users
+CHANGE COLUMN emai email VARCHAR(255) NOT NULL UNIQUE;
+
+
+-- @block // Changing the name of the table to 'Users' and deleting one of the duplicated 'email' columns
+ALTER TABLE users
+DROP COLUMN email;
+
+
+-- @block // Show the column fields
+DESCRIBE Users;
+
+
+-- @block // Drop the whole table
+DROP TABLE users;
